0690b6beed668b83771ffb952653ba2f583632e0
james33344 <james455096@gmail.com>
2015-06-05 02:00:03 +0800

    kernel: Add IRQ control macros

--- include/kernel.h
+++ include/kernel.h
@@ -6,4 +6,10 @@
 #include "event-monitor.h"
 #include <stddef.h>
 
+#define INTR_EVENT(intr) (FILE_LIMIT + (intr) + 15) /* see INTR_LIMIT */
+#define INTR_EVENT_REVERSE(event) ((event) - FILE_LIMIT - 15)
+#define TIME_EVENT (FILE_LIMIT + INTR_LIMIT)
+#define _disable_irq() __asm__ __volatile__ ( "cpsid i" );
+#define _enable_irq() __asm__ __volatile__ ( "cpsie i" );
+
 #endif
