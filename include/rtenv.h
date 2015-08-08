797868924506c8125406bb1c3f473a4c98c91786
lecopzer <james455096@gmail.com>
2015-08-08 21:47:45 +0800

    Add rtenv.h including necessary header file for user apps

---
+++ include/rtenv.h
@@ -0,0 +1,30 @@
+#ifndef RTENV_H
+#define RTENV_H
+
+#include <stddef.h>
+#include <ctype.h>
+
+#include "task.h"
+#include "pthread.h"
+#include "malloc.h"
+
+#include "kconfig.h"
+#include "kernel.h"
+
+#include "stm32_p103.h"
+#include "stm32f10x_conf.h"
+#include "RTOSConfig.h"
+
+#include "string.h"
+#include "file.h"
+
+#include "mqueue.h"
+#include "syscall.h"
+
+#include "path.h"
+#include "fifo.h"
+
+
+
+
+#endif
