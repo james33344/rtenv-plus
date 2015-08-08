3611bd1d7d21c18ce8b34eb55f6b53da4cd74a5e
lecopzer <james455096@gmail.com>
2015-08-08 21:51:46 +0800

    string.h: Add puts()

--- include/string.h
+++ include/string.h
@@ -8,5 +8,7 @@ int strcmp(const char *a, const char *b) __attribute__ ((naked));
 size_t strlen(const char *s) __attribute__ ((naked));
 char *strcpy(char *dest, const char *src);
 int strncmp(const char *s1, const char *s2, size_t n);
+void puts(char *s);
+
 
 #endif
