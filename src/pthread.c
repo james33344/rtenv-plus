e1f54a7175b67129cba155441b8d460ad540569f
lecopzer <james455096@gmail.com>
2015-08-08 21:52:58 +0800

    pthread: Add pthread_exit, pthread_equl
    Let pthread_create first arg 'thread' work for pthread_equl(),
    by point to thread tcb

--- src/pthread.c
+++ src/pthread.c
@@ -3,19 +3,44 @@
 #include "kconfig.h"
 #include "task.h"
 #include "syscall.h"
+#include "malloc.h"
 
-
+extern struct task_control_block* current_tcb;
 
 int pthread_create(pthread_t *restrict thread,
 					const pthread_attr_t *restrict attr,
 			        void *(*start_routine)(void*), 
 					void *restrict arg) {
-		
-	task_create(0, start_routine, NULL);
 
-	return 1;
+	struct task_control_block* tcb = 0;
+	if (!attr) {
+		tcb = task_create(PRIORITY_DEFAULT, start_routine, NULL);
+	}
+	else {
+		tcb = task_create(attr->sched_param.sched_priority, start_routine, NULL);
+	}
+
+	if (tcb == NULL) return 1;
+
+	(*thread)->tcb = tcb;
+
+	return 0;
 }
 
 pthread_t pthread_self() {
-	return getpid();
+	pthread_t retval = (pthread_t) malloc(sizeof(pthread_t));
+	retval->tcb = current_tcb;
+	return retval;
+}
+
+int pthread_equal(pthread_t t1, pthread_t t2) {
+	if(t1->tcb == t2->tcb) {
+		return 1;
+	}
+	return 0;
+}
+
+
+void pthread_exit(void *value_ptr) {
+	task_exit(NULL);
 }
